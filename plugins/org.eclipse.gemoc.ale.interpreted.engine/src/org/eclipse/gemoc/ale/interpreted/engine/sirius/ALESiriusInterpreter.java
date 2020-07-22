d0cf00db07eb6e34e2221ca97da888f5b6ce2ff2
Didier Vojtisek <didier.vojtisek@inria.fr>
2020-07-22 09:08:30 +0200

    improve support of ale RTD animation for multiple engine
    use an EngineAddon to register/unregister engines that are used by the
    ALESiriusInterpreter
    
    Enable ALESiriusInterpreter in edition mode (in addition to run mode)
    
    fix https://github.com/eclipse/gemoc-studio-execution-ale/issues/44
    
    Signed-off-by: Didier Vojtisek <didier.vojtisek@inria.fr>

--- plugins/org.eclipse.gemoc.ale.interpreted.engine/src/org/eclipse/gemoc/ale/interpreted/engine/sirius/ALESiriusInterpreter.java
+++ plugins/org.eclipse.gemoc.ale.interpreted.engine/src/org/eclipse/gemoc/ale/interpreted/engine/sirius/ALESiriusInterpreter.java
@@ -1,5 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2018, 2020 Inria and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *     Inria - refactoring and improvements
+ *******************************************************************************/
 package org.eclipse.gemoc.ale.interpreted.engine.sirius;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -13,19 +27,31 @@ import org.eclipse.acceleo.query.runtime.QueryParsing;
 import org.eclipse.emf.common.util.BasicDiagnostic;
 import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecoretools.ale.core.interpreter.impl.AleInterpreter;
 import org.eclipse.emf.ecoretools.ale.core.interpreter.impl.OptimizedEvaluationResult;
 import org.eclipse.gemoc.ale.interpreted.engine.AleEngine;
 import org.eclipse.sirius.common.acceleo.aql.business.internal.AQLSiriusInterpreter;
 import org.eclipse.sirius.common.tools.api.interpreter.EvaluationException;
 import org.eclipse.sirius.common.tools.api.interpreter.IEvaluationResult;
+import org.eclipse.xtext.EcoreUtil2;
 
 public class ALESiriusInterpreter extends AQLSiriusInterpreter {
 
-	AleEngine engine;
+	/**
+	 * list of known engines that can be used to evalutate the expressions
+	 */
+	HashSet<AleEngine> knownEngines = new HashSet<>();
 
-	public ALESiriusInterpreter(AleEngine engine) {
-		this.engine = engine;
+	// ALESiriusInterpreter is a singleton
+	private static ALESiriusInterpreter instance = new ALESiriusInterpreter();
+
+	private ALESiriusInterpreter() {
+	}
+
+	public static ALESiriusInterpreter getDefault() {
+		return instance;
 	}
 
 	@Override
@@ -41,28 +67,90 @@ public class ALESiriusInterpreter extends AQLSiriusInterpreter {
 		Map<String, Object> variables = getVariables();
 		variables.put("self", target); //$NON-NLS-1$
 
-		AleInterpreter aleInterpreter = engine.getInterpreter();
-		if (aleInterpreter != null) {
-			IQueryEnvironment queryEnv = aleInterpreter.getQueryEnvironment();
-			if (queryEnv != null) {
-				final IQueryBuilderEngine builder = QueryParsing.newBuilder(queryEnv);
-				AstResult build = builder.build(expression);
-				IQueryEvaluationEngine evaluationEngine = QueryEvaluation.newEngine(queryEnv);
-				final EvaluationResult evalResult = evaluationEngine.eval(build, variables);
+		IQueryEnvironment queryEnv = getQueryEnvironment(target);
+		final IQueryBuilderEngine builder = QueryParsing.newBuilder(queryEnv);
+		AstResult build = builder.build(expression);
+		IQueryEvaluationEngine evaluationEngine = QueryEvaluation.newEngine(queryEnv);
+		final EvaluationResult evalResult = evaluationEngine.eval(build, variables);
 
-				final BasicDiagnostic diagnostic = new BasicDiagnostic();
-				if (Diagnostic.OK != build.getDiagnostic().getSeverity()) {
-					diagnostic.merge(build.getDiagnostic());
-				}
-				if (Diagnostic.OK != evalResult.getDiagnostic().getSeverity()) {
-					diagnostic.merge(evalResult.getDiagnostic());
-				}
-				return new OptimizedEvaluationResult(
-						Optional.ofNullable(evalResult.getResult()),
-						diagnostic);
+		final BasicDiagnostic diagnostic = new BasicDiagnostic();
+		if (Diagnostic.OK != build.getDiagnostic().getSeverity()) {
+			diagnostic.merge(build.getDiagnostic());
+		}
+		if (Diagnostic.OK != evalResult.getDiagnostic().getSeverity()) {
+			diagnostic.merge(evalResult.getDiagnostic());
+		}
+		if (queryEnv instanceof BasicEditionRawAleEnvironment) {
+			// release resources
+			((BasicEditionRawAleEnvironment) queryEnv).close();
+		}
+		return new OptimizedEvaluationResult(Optional.ofNullable(evalResult.getResult()), diagnostic);
+	}
+
+	/**
+	 * retrieve the queryEnvironment from the associated engine or create a
+	 * BasicEditionRawAleEnvironment (for edition mode) Important:
+	 * BasicEditionRawAleEnvironment must be closed after use, in order to avoid
+	 * memory leak
+	 */
+	@SuppressWarnings("resource")
+	private IQueryEnvironment getQueryEnvironment(final EObject target) {
+		Optional<AleInterpreter> aleInterpreter = findInterpreterForModel(target);
+		if (aleInterpreter.isPresent() && aleInterpreter.get().getQueryEnvironment() != null) {
+			return aleInterpreter.get().getQueryEnvironment();
+		} else {
+			List<EPackage> metamodels = getMetamodels(target);
+			IQueryEnvironment queryEnv = new BasicEditionRawAleEnvironment(metamodels).getContext();
+			for (EPackage pack : metamodels) {
+				queryEnv.registerEPackage(pack);
+			}
+			return queryEnv;
+		}
+	}
+
+	/**
+	 * Look in known engines that are running the model containing the given target
+	 * EObject
+	 * 
+	 * @param target
+	 * @return the AleInterpreter of the found AleEngine
+	 */
+	protected Optional<AleInterpreter> findInterpreterForModel(final EObject target) {
+		for (AleEngine engine : knownEngines) {
+			if (engine.getExecutionContext().getResourceModel().getResourceSet() == target.eResource()
+					.getResourceSet()) {
+				return Optional.ofNullable(engine.getInterpreter());
 			}
 		}
+		return Optional.empty();
+	}
 
-		return org.eclipse.sirius.common.tools.api.interpreter.EvaluationResult.ofValue(null);
+	/**
+	 * reflectively find the metamodels of the given EObject
+	 * 
+	 * Ie. look for all EPackage of all Resources in the ResourceSet
+	 * 
+	 * @param target
+	 * @return a list of EPackage
+	 */
+	protected List<EPackage> getMetamodels(final EObject target) {
+		ArrayList<EPackage> metamodels = new ArrayList<EPackage>();
+		for (Resource res : target.eResource().getResourceSet().getResources()) {
+			// TODO find a more efficient request ? (ie. avoid to navigate the whole model )
+			// could we make the assumption that EPackage are only at the root or contained
+			// by EPackage ?
+			// (DVK: I know that UML may use EPackage in order to create profiles
+			// instantiation)
+			metamodels.addAll(EcoreUtil2.typeSelect(EcoreUtil2.eAllContentsAsList(res), EPackage.class));
+		}
+		return metamodels;
+	}
+
+	public void addAleEngine(AleEngine engine) {
+		knownEngines.add(engine);
+	}
+
+	public void removeAleEngine(AleEngine engine) {
+		knownEngines.remove(engine);
 	}
 }
